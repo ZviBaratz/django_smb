c2ff37c6c0577b42d5ed4e783ab5d1aaafb63f74
Zvi Baratz <z.baratz@gmail.com>
2019-04-16 19:58:50 +0300

    Added some docstrings

--- django_smb/models/remote_location.py
+++ django_smb/models/remote_location.py
@@ -9,42 +9,118 @@ from django_smb.models import RemotePath
 
 
 class RemoteLocation(models.Model):
-    name = models.CharField(max_length=64, blank=False, unique=True)
-    user_id = models.CharField(max_length=64, blank=False)
-    password = models.CharField(max_length=64, blank=False)
-    share_name = models.CharField(max_length=64, blank=False)
-    server_name = models.CharField(max_length=64, blank=False, unique=True)
-    last_sync = models.DateTimeField(null=True, blank=True)
+    """
+    A model to represent a remote Windows Share directory.
+    
+    """
 
+    name = models.CharField(
+        max_length=64,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text="A unique name for the remote location.",
+    )
+    user_id = models.CharField(
+        max_length=64,
+        blank=False,
+        null=False,
+        help_text="The name of the user that is meant to be logged in.",
+    )
+    password = models.CharField(
+        max_length=64,
+        blank=False,
+        null=False,
+        help_text="The password of the user that is meant to be logged in.",
+    )
+    share_name = models.CharField(
+        max_length=64,
+        blank=False,
+        null=True,
+        help_text="The name of the shared location.",
+    )
+    server_name = models.CharField(
+        max_length=64,
+        blank=False,
+        null=False,
+        help_text="The name of the server on which the shared location is found.",
+    )
+    last_sync = models.DateTimeField(
+        blank=True, null=True, help_text="Time of the last synchronization."
+    )
+
+    # This field is meant to represent the root directory of the remote location.
     tree_root = models.OneToOneField(
         RemotePath,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name='root_for',
+        related_name="root_for",
     )
 
     class Meta:
         verbose_name_plural = "Locations"
 
+    def __str__(self) -> str:
+        return self.name
+
     def get_absolute_url(self):
-        return reverse('smb:locations')
+        return reverse("smb:locations")
 
     def get_server_ip(self) -> str:
+        """
+        Returns the IPv4 address of the server as a string.
+        
+        Returns
+        -------
+        str
+            SMB Server IPv4 address.
+        """
+
         return socket.getaddrinfo(self.server_name, 139)[0][-1][0]
 
     def get_client_name(self) -> str:
+        """
+        Return the name of this computer (the client).
+        
+        Returns
+        -------
+        str
+            Client's name.
+        """
+
         return socket.gethostname()
 
     def create_connection(self) -> SMBConnection:
+        """
+        Uses pysmb_ to create and return a configured SMBConnection_ instance.
+
+        .. _pysmb: https://github.com/miketeo/pysmb
+        .. _SMBConnection: https://github.com/miketeo/pysmb/blob/131bc9cafa967c362a360685aa8c7086e9b26312/sphinx/source/api/smb_SMBConnection.rst
+        
+        Returns
+        -------
+        SMBConnection
+            A potential connection to the remote location using SMB.
+        """
+
         return SMBConnection(
-            self.user_id,
-            self.password,
-            self.get_client_name(),
-            self.server_name,
+            self.user_id, self.password, self.get_client_name(), self.server_name
         )
 
     def connect(self) -> SMBConnection:
+        """
+        Create an SMBConnection_ instance and call its connect() method with the
+        remote location's server IP address.
+
+        .. _SMBConnection: https://github.com/miketeo/pysmb/blob/131bc9cafa967c362a360685aa8c7086e9b26312/sphinx/source/api/smb_SMBConnection.rst
+        
+        Returns
+        -------
+        SMBConnection
+            An active connection to the remote location using SMB.
+        """
+
         conn = self.create_connection()
         ip_address = self.get_server_ip()
         # This function is called also when checking whether the directory
@@ -57,7 +133,21 @@ class RemoteLocation(models.Model):
         except OSError:
             return False
 
-    def list_files(self, path: str):
+    def list_files(self, path: str) -> list:
+        """
+        Lists the files within a remote location's relative path.
+        
+        Parameters
+        ----------
+        path : str
+            The relative path of the location to be listed.
+        
+        Returns
+        -------
+        list
+            A list of the files and directories in the given location.
+        """
+
         connection = self.connect()
         if connection:
             paths = connection.listPath(self.share_name, path)[2:]
@@ -65,6 +155,21 @@ class RemoteLocation(models.Model):
             return paths
 
     def list_files_recursively(self, path: str) -> list:
+        """
+        List all the files found under the remote location's specified path and
+        sub-directories.
+        
+        Parameters
+        ----------
+        path : str
+            The relative path of the location to be listed.
+        
+        Returns
+        -------
+        list
+            All files found within the given path and its sub-directories.
+        """
+
         files = self.list_files(path)
         result = []
         for f in files:
@@ -76,30 +181,51 @@ class RemoteLocation(models.Model):
         return result
 
     def list_all_paths(self) -> list:
-        return self.list_files_recursively('.')
+        """
+        List the relative paths of all the files found within the remote location
+        (under its root directory).
+        
+        Returns
+        -------
+        list
+            All relative file paths within the remote location.
+        """
+
+        return self.list_files_recursively(".")
 
     def create_tree_root(self):
-        new_root = RemotePath.add_root(name='.')
+        new_root = RemotePath.add_root(name=".")
         new_root.save()
         self.tree_root = new_root
         self.save()
         return self.tree_root
 
-    def sync(self) -> bool:
+    def sync(self) -> None:
+        """
+        Synchronize the root node to update with any new remote files.
+        
+        """
+
         if self.tree_root is None:
             self.create_tree_root()
         self.tree_root.sync()
         self.last_sync = datetime.now()
         self.save()
-        return True
 
     @property
-    def is_connected(self):
+    def is_connected(self) -> bool:
+        """
+        Check if a connection can be made to the remote location.
+        
+        Returns
+        -------
+        bool
+            Whether the connection was successful.
+        """
+
         connection = self.connect()
         if connection:
             connection.close()
             return True
         return False
 
-    def __str__(self):
-        return self.name
--- django_smb/models/remote_path.py
+++ django_smb/models/remote_path.py
@@ -7,54 +7,94 @@ from django.urls import reverse
 from treebeard.mp_tree import MP_Node
 from smb.smb_structs import OperationFailure
 
-ICON_DICT = {
-    '': 'fas fa-server',
-    'dcm': 'fab fa-magento',
-}
+ICON_DICT = {"": "fas fa-server", "dcm": "fab fa-magento"}
 
 
 class RemotePath(MP_Node):
-    name = models.CharField(max_length=100, blank=False, null=False)
-    created = models.DateTimeField(auto_now_add=True)
-    is_imported = models.BooleanField(default=False)
-    node_order_by = ['name']
+    """
+    A treebeard_'s `materialized path node`_ model to represent a remote path as part of a tree.
+    This is meant to make the model's management easier and more efficient.
+    
+    .. _treebeard: https://github.com/django-treebeard/django-treebeard
+    .. _materialized path node: https://django-treebeard.readthedocs.io/en/latest/mp_tree.html
+    """
+
+    name = models.CharField(
+        max_length=100, blank=False, null=False, help_text="The file or directory name."
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        help_text="The time in which the node representing this file or directory was created.",
+    )
+    is_imported = models.BooleanField(
+        default=False,
+        help_text="Whether this file had been imported from the remote location to the client or not.",
+    )
+    node_order_by = ["name"]
 
     def get_absolute_url(self):
-        return reverse('smb:locations')
+        return reverse("smb:locations")
 
-    def to_dict(self, lazy=True) -> dict:
-        d = {
-            'id': str(self.id),
-            'text': self.name,
-        }
-        if self.name is '.':
-            d['text'] = self.root_for.name
+    def to_dict(self, lazy: bool = True) -> dict:
+        """
+        Returns a dictionary meant to be used by jstree to visualize the directory
+        tree. If lazy is set to True, will return only this node's dictionary,
+        otherwise will run recursively and return a nested dictionary.
+        
+        Parameters
+        ----------
+        lazy : bool, optional
+            Return only this node or nest children (the default is True, which will return just this node).
+        
+        Returns
+        -------
+        dict
+            This node's representation as a dictionary for jstree.
+        """
 
-        ext = self.name.split('.')[-1]
+        d = {"id": str(self.id), "text": self.name}
+        if self.name == ".":
+            d["text"] = self.root_for.name
+
+        ext = self.name.split(".")[-1]
         if ext not in ICON_DICT or not ext:
             if lazy or not self.get_children():
-                d['children'] = True
+                d["children"] = True
             else:
-                d['children'] = [
-                    child.to_dict(lazy=False)
-                    for child in self.get_children().all()
+                d["children"] = [
+                    child.to_dict(lazy=False) for child in self.get_children().all()
                 ]
         else:
-            d['children'] = False
+            d["children"] = False
 
-        d['icon'] = ICON_DICT.get(ext, 'fas fa-folder')
+        d["icon"] = ICON_DICT.get(ext, "fas fa-folder")
         if self.is_available:
-            d['icon'] += ' available'
+            d["icon"] += " available"
         else:
-            d['icon'] += ' unavailable'
-        if 'folder' not in d['icon'] and 'server' not in d['icon']:
+            d["icon"] += " unavailable"
+        if "folder" not in d["icon"] and "server" not in d["icon"]:
             if self.is_imported:
-                d['icon'] += ' imported'
+                d["icon"] += " imported"
             else:
-                d['icon'] += ' notimported'
+                d["icon"] += " notimported"
         return d
 
-    def sync(self, lazy=False):
+    def sync(self, lazy: bool = False) -> bool:
+        """
+        Checks for unfamiliar files under this node. Running in lazy mode will run
+        flatly instead of recursively.
+        
+        Parameters
+        ----------
+        lazy : bool, optional
+            Run lazily or recursively (the default is False, which will run recursively).
+        
+        Returns
+        -------
+        bool
+            Whether the synchronization was successful or not.
+        """
+
         location = self.get_root().root_for
         current_files = location.list_files(self.full_path)
         if current_files is not None:
@@ -70,34 +110,64 @@ class RemotePath(MP_Node):
             return True
         return False
 
-    def get_file(self):
+    def get_file(self) -> tempfile.NamedTemporaryFile:
+        """
+        Return the contents of a remote file.
+        
+        Returns
+        -------
+        tempfile.NamedTemporaryFile
+            A temporary file copied from the remote location.
+        """
+
         location = self.get_root().root_for
         connection = location.connect()
         temp_file = tempfile.NamedTemporaryFile()
-        connection.retrieveFile(
-            location.share_name,
-            self.full_path,
-            temp_file,
-        )
+        connection.retrieveFile(location.share_name, self.full_path, temp_file)
         connection.close()
         temp_file.seek(0)
         return temp_file
 
     @property
-    def location_path(self):
+    def location_path(self) -> str:
+        """
+        Compose the relative path of this node's location using its ancestors.
+        
+        Returns
+        -------
+        str
+            The relative path of this node's directory within the remote location.
+        """
+
         if self.is_root():
             return self.name
-        return os.path.join(
-            *[ancestor.name for ancestor in self.get_ancestors()])
+        return os.path.join(*[ancestor.name for ancestor in self.get_ancestors()])
 
     @property
-    def full_path(self):
+    def full_path(self) -> str:
+        """
+        Compose the full (relative) path of this node using its ancestors.
+        
+        Returns
+        -------
+        str
+            This node's path within the remote location.
+        """
         if self.is_root():
             return self.name
         return os.path.join(self.location_path, self.name)
 
     @property
-    def is_available(self):
+    def is_available(self) -> bool:
+        """
+        Checks whether this node still exists within the remote location.
+        
+        Returns
+        -------
+        bool
+            Node availability (existence).
+        """
+
         location = self.get_root().root_for
         try:
             dir_files = location.list_files(self.location_path)
@@ -109,6 +179,3 @@ class RemotePath(MP_Node):
         if self.name in file_names:
             return True
         return False
-
-
-RemotePath._meta.get_field('path').max_length = 1024
